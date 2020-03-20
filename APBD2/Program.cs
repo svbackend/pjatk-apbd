17b442b65688fd7f450fb643f91c58a82d2ceb20
Valentyn Saik <svbackend22@gmail.com>
2020-03-20 15:16:02 +0100

    Init 1st programming task

---
+++ APBD2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace APBD2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFilePath = args.Length > 0 ? args[0] : "data.csv";
+            var outputFilePath = args.Length > 1 ? args[1] : "result.xml";
+            var format = args.Length > 2 ? args[2] : "xml";
+            
+            // todo
+        }
+    }
+}
