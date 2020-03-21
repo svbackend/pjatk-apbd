b027a183b3b222820ca98461154b3a98b7e0ee08
Valentyn Saik <svbackend22@gmail.com>
2020-03-21 13:09:51 +0100

    Added studentStudies wrapper for correct xml serialization

--- APBD2/Entity/StudentStudies.cs
+++ APBD2/Entity/StudentStudies.cs
@@ -1,7 +1,15 @@
+using System;
+using System.Xml.Serialization;
+
 namespace APBD2.Entity
 {
+    [Serializable]
     public class StudentStudies
     {
+        [XmlElement (ElementName = "name")]
+        public string Name { get; set; }
         
+        [XmlElement (ElementName = "mode")]
+        public string Mode { get; set; }
     }
 }
