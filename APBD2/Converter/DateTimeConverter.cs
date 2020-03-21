68ea66a375e927826e36989675be9792c10a5b01
Valentyn Saik <svbackend22@gmail.com>
2020-03-21 13:09:19 +0100

    Added json date time converter

--- APBD2/Converter/DateTimeConverter.cs
+++ APBD2/Converter/DateTimeConverter.cs
@@ -1,14 +1,20 @@
-using System.ComponentModel;
-using System.Runtime.Serialization;
-using Microsoft.VisualBasic;
+using System;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace APBD2.Converter
 {
-    public class DateFormatConverter : DateTimeConverter
+    public class DateFormatConverter : JsonConverter<DateTime>
     {
-        DateFormatConverter()
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            DateFormat = "";
+            throw new NotImplementedException();
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture));
         }
     }
 }
