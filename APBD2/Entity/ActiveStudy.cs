151ae2cecfea051c6fbabfda802ebb8d14145472
Valentyn Saik <svbackend22@gmail.com>
2020-03-21 13:07:53 +0100

    ActiveStudy entity added

--- APBD2/Entity/ActiveStudy.cs
+++ APBD2/Entity/ActiveStudy.cs
@@ -1,7 +1,14 @@
+using System.Xml.Serialization;
+
 namespace APBD2.Entity
 {
+    [XmlRoot(ElementName = "studies")]
     public class ActiveStudy
     {
-        
+        [XmlAttribute]
+        public string Name { get; set; }
+
+        [XmlAttribute]
+        public int NumberOfStudents { get; set; }
     }
 }
